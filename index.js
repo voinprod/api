730e79c4479c4c82febf0a561a8c21f558bf0c96
voinprod <workpetua@protonmail.com>
2018-11-12 18:43:41 +0300

    connect

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mogoose = require('mongoose');
+
+
+mongoose.connect('');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+
+
+app.listen(port, () => {
+    console.log('Work on port ' + port);
+})
